13c96f7b4259cb1cbdc3fd6da3f81e5d70f5f4f8
Aleksander <Greengow@mail.ru>
2021-02-06 17:07:51 +0300

    Hello World Final

---
+++ TMS.Net07.Homework.HelloWorld/TMS.Net07.Homework.HelloWorld/TMS.Net07.Homework/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScapeGoat:
+            Console.WriteLine("Введите число");
+            string somfin = Console.ReadLine(); ;
+            int number;
+            {
+                Console.Clear();
+                if (int.TryParse(somfin, out number))
+                {
+                    Console.WriteLine($"Введено число {number}");
+                    if (number != 0)
+                    {
+                        Console.WriteLine("Продолжайте " +
+                              "для завершения работы введите 0");
+                        goto ScapeGoat;
+                    }
+                    else 
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Это не число");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
